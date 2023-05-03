cb0bc3488624964271faa9a40c0cb25fc2463f21
nickwu63 <wulumin0603@gmail.com>
2023-05-03 12:01:57 +0800

    add week12

---
+++ week12-1_printf_fprintf.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{///檔案指標 fout = 開檔案("檔名", "用甚麼模式");
+    FILE * fout = fopen("file.txt", "w");///:write
+
+    printf("Hello World\n");
+    fprintf(fout,"Hello World在檔案裡\n");;
+}
